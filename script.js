c7fa0be17a9ebcca7191f3958af74aa2a8b93d58
lucio <creppenderman@gmail.com>
2023-09-20 17:00:37 -0300

    juego crear mueble

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 class Auto{
-    patenteAnterior = 0
     constructor(modelo,año,color,precio){
         this.marca = "Jaguar";
         this.modelo = modelo;
         this.año = año;
         this.color = color;
         this.precio = precio;
-        this.patente = this.patenteAnterior+1
-        this.patenteAnterior++
+        this.patente = this.patenteAnterior+1;
+        this.encendido = false;
+        this.velocidad = 0;
+        this.km = 0;
     }
+    arrancar(){
+        this.encendido = true;
+    }
+    acelerar(aceleracion){
+        if (this.encendido){
+            this.velocidad += aceleracion;
+        }
+        else{
+            console.log("el auto no está encendido")
+        }
+        
+    }
+    frenar(){
+        if (this.velocidad>100){
+            this.#chocar()
+        }
+        this.velocidad = 0;
+    }
+    #chocar(){
+        this.color = null;
+        this.precio = 0;
+        this.encendido = false;
+        this.km = 0;
+    }
+
 }
 
 class Torta{
@@ -22,17 +48,49 @@ class Torta{
         this.forma = "redonda"
     }
 }
-
-
 class Usuario{
-    cantidad_usuarios = 0
     constructor(nickname,nombre,apellido,email,contraseña){
-        this.id = cantidad_usuarios
+        // this.id = cantidad_usuarios
         this.nickname = nickname
         this.nombre = nombre
         this.apellido = apellido
         this.email = email
         this.contraseña = contraseña
-        this.cantidad_usuarios++
+        // cantidad_usuarios++
     }
 }
+
+
+class Mueble{
+    constructor(nombre,ancho,alto,peso,material="madera",color="gray"){
+        this.nombre = nombre
+        this.ancho = ancho
+        this.alto = alto
+        this.peso = peso
+        this.material = material
+        this.color = color
+
+        this.#crear_elemento()
+    }
+    #crear_elemento(){
+        // crear elemento
+        this.div = document.createElement("div")
+        this.p_nombre = document.createElement("p")
+        this.p_peso = document.createElement("p")
+
+        this.div.classList.add("mueble")
+        this.div.style.width = this.ancho +"px"
+        this.div.style.height = this.alto +"px"
+        this.div.style.backgroundColor = this.color
+
+        this.p_nombre.innerText = this.nombre
+        this.p_peso.innerText = this.peso +"kg"
+        
+        this.div.appendChild(this.p_nombre)
+        this.div.appendChild(this.p_peso)
+        document.querySelector("#grilla").appendChild(this.div)
+    }
+
+}
+
+escritorio = new Mueble("escritorio",200,100,100,"madera","brown")
